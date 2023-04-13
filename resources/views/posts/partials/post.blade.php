8113e7fcc1ad544e3ccab9745c7e77997a2a90b6
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-04-13 20:57:50 +0700

    27th: s17_v81_style_for_post_list_page

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,19 +1,22 @@
 {{-- c1: cách này tương ứng với c1 bên ../index.blade.php --}}
-@if($loop->even)
+{{-- @if($loop->even)
   <div style='background-color:gray'>{{ $key }} - {{ $post['title'] }}</div>
 @else
   <div>{{ $key }} - {{ $post['title'] }}</div>
-@endif
+@endif --}}
+
+<h3><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a></h3>
 
 {{-- c2: cách này tương ứng với c2 bên ../index.blade.php --}}
 {{-- <p>{{ $key }} - {{ $post ['title'] }}</p> --}}
 
 
 
-<div>
-  <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+<div class="mb-3">
+  <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
+  <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
     @csrf
     @method('DELETE')
-    <input type="submit" value="Delete">
+    <input type="submit" value="Delete" class="btn btn-primary">
   </form>
 </div>
