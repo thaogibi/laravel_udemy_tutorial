bee305bcfab473393f9cccc2ee3c140512bff0fc
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-04-21 02:58:04 +0700

    40th: s23_v112_formatting_validation_errors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,22 +8,43 @@
 
     <div class="form-group">
       <label>Name</label>
-      <input name="name" value="{{ old('name') }}" required class="form-control">
+      <input type="text" name="name" value="{{ old('name') }}" required 
+        class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}">
+
+        @if($errors->has('name'))
+          <span class="invalid-feedback">
+            <strong>{{ $errors->first('name') }}</strong>
+          </span>
+        @endif
     </div>
 
     <div class="form-group">
       <label>E-mail</label>
-      <input name="email" value="{{ old('email') }}" required class="form-control">
+      <input type="email" name="email" value="{{ old('email') }}" required 
+        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}">
+
+        @if($errors->has('email'))
+          <span class="invalid-feedback">
+            <strong>{{ $errors->first('email') }}</strong>
+          </span>
+        @endif
     </div>
 
     <div class="form-group">
       <label>Password</label>
-      <input name="password" required class="form-control">
+      <input type="password" name="password" required 
+        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
+
+        @if($errors->has('password'))
+          <span class="invalid-feedback">
+            <strong>{{ $errors->first('password') }}</strong>
+          </span>
+        @endif
     </div>
 
     <div class="form-group">
       <label>Retyped Password</label>
-      <input name="password_confirmation" required class="form-control">
+      <input type="password" name="password_confirmation" required class="form-control">
     </div>
 
     <button type="submit" class="btn btn-primary btn-block">Register!</button>
