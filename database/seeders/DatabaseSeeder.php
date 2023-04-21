fb21ccbcdb43e3f616c76ddb7f2a9bcc969376b4
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-04-21 22:53:27 +0700

    49th: s24_v121_database_seeding_basics

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; //them vao 
+use Illuminate\Support\Str;//them vao
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'name' => 'meomeo',
+            'email' => 'meomeo@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()->create([
+            'name' => 'meomeo2',
+            'email' => 'meomeo2@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
