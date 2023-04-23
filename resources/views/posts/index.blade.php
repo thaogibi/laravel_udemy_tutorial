3a1f3593a40c1cc844567adff7f35f64b41de244
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-04-23 22:17:09 +0700

    77th: s28_v153_Practical_creating_reusable_component_for_dates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,78 +3,105 @@
 @section('title', 'List posts')
 
 @section('content')
-  <div class="row">
-    <div class="col-8">
-      <h1>List posts</h1>
+    <div class="row">
+        <div class="col-8">
+        <h1>List posts</h1>
 
-      {{-- c1: nếu dùng cách này thì uncomment 1-5 bên post.blade.php --}}
-      @forelse($posts as $key => $post)
-        @include('posts.partials.post')
-      @empty
-        <p>Not found posts</p>
-      @endforelse
-      {{-- c2: nếu dùng cách này thì uncomment dòng 6 bên post.blade.php --}}
-      {{-- @each('posts.partials.post', $posts, 'post')      --}}
-    </div>
-
-
-
-  <div class="col-4">
-    <div class="container">
-      <div class="row">
-          <div class="card" style="width: 100%;">
-              <div class="card-body">
-                  <h5 class="card-title">Most Commented</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">
-                      What people are currently talking about
-                  </h6>
-              </div>
-              <ul class="list-group list-group-flush">
-                  {{-- @foreach ($mostCommented as $post)
-                      <li class="list-group-item">
-                          <a href="{{ route('posts.show', ['post' => $post->id]) }}">
-                              {{ $post->title }}
-                          </a>
-                      </li>
-                  @endforeach --}}
-              </ul>
-          </div>
-      </div>
-      <div class="row mt-4">
-          <div class="card" style="width: 100%;">
-              <div class="card-body">
-                  <h5 class="card-title">Most Active</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">
-                      Users with most posts written
-                  </h6>
-              </div>
-              <ul class="list-group list-group-flush">
-                  {{-- @foreach ($mostActive as $user)
-                      <li class="list-group-item">
-                          {{ $user->name }}
-                      </li>
-                  @endforeach --}}
-              </ul>
-          </div>
-      </div>
-      <div class="row mt-4">
-        <div class="card" style="width: 100%;">
-            <div class="card-body">
-                <h5 class="card-title">Most Active Last Month</h5>
-                <h6 class="card-subtitle mb-2 text-muted">
-                    Users with most posts written in the month
-                </h6>
-            </div>
-            <ul class="list-group list-group-flush">
-                {{-- @foreach ($mostActiveLastMonth as $user)
-                    <li class="list-group-item">
-                        {{ $user->name }}
-                    </li>
-                @endforeach --}}
-            </ul>
+        {{-- c1: nếu dùng cách này thì uncomment 1-5 bên post.blade.php --}}
+        @forelse($posts as $key => $post)
+            @include('posts.partials.post')
+        @empty
+            <p>Not found posts</p>
+        @endforelse
+        {{-- c2: nếu dùng cách này thì uncomment dòng 6 bên post.blade.php --}}
+        {{-- @each('posts.partials.post', $posts, 'post')      --}}
         </div>
-      </div>
+
+
+
+    <div class="col-4">
+        <div class="container">
+        <div class="row">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">Most Commented</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                        What people are currently talking about
+                    </h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    {{-- @foreach ($mostCommented as $post)
+                        <li class="list-group-item">
+                            <a href="{{ route('posts.show', ['post' => $post->id]) }}">
+                                {{ $post->title }}
+                            </a>
+                        </li>
+                    @endforeach --}}
+                </ul>
+            </div>
+            {{-- @card(['title' => 'Most Commented'])
+                @slot('subtitle')
+                    What people are currently talking about
+                @endslot
+                @slot('items')
+                    @foreach ($mostCommented as $post)
+                        <li class="list-group-item">
+                            <a href="{{ route('posts.show', ['post' => $post->id]) }}">
+                                {{ $post->title }}
+                            </a>
+                        </li>
+                    @endforeach
+                @endslot
+            @endcard --}}
+        </div>
+        
+        <div class="row mt-4">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">Most Active</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                        Users with most posts written
+                    </h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    {{-- @foreach ($mostActive as $user)
+                        <li class="list-group-item">
+                            {{ $user->name }}
+                        </li>
+                    @endforeach --}}
+                </ul>
+            </div>
+            {{-- @card(['title' => 'Most Active'])
+                @slot('subtitle')
+                    Writers with most posts written
+                @endslot
+                @slot('items', collect($mostActive)->pluck('name'))
+            @endcard --}}
+        </div>
+        <div class="row mt-4">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">Most Active Last Month</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                        Users with most posts written in the month
+                    </h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    {{-- @foreach ($mostActiveLastMonth as $user)
+                        <li class="list-group-item">
+                            {{ $user->name }}
+                        </li>
+                    @endforeach --}}
+                </ul>
+            </div>
+            {{-- @card(['title' => 'Most Active Last Month'])
+                @slot('subtitle')
+                    Users with most posts written in the month
+                @endslot
+                @slot('items', collect($mostActiveLastMonth)->pluck('name'))
+            @endcard --}}
+        </div>
+        </div>
     </div>
-  </div>
-</div>
+    </div>
 @endsection
